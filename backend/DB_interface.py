a32ee3da59a97061cdf25a4693efce57635e6f0f
Akash-Balaji003 <akashbalaji.tvr@gmail.com>
2024-10-14 02:16:54 +0530

    Major Modification version 1

--- backend/DB_interface.py
+++ backend/DB_interface.py
@@ -1,7 +1,73 @@
-from datetime import date
-from typing import List
+from datetime import date, datetime
+from typing import Union, List, Dict
+from fastapi import HTTPException
 import mysql.connector
 import json
+
+def get_fruit_records():
+    # Create the connection to the MySQL database
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='Hack',
+            user='root',
+            password='Akash003!'
+        )
+
+        if connection.is_connected():
+            print('Connected to MySQL database')
+
+            # Create a cursor object
+            cursor = connection.cursor(dictionary=True)
+
+            # SQL query to fetch fruits where Expiry_date is NULL
+            query = "SELECT item_name, item_id FROM fruits_spoilage;"
+            cursor.execute(query)
+            result = cursor.fetchall()
+
+            return result
+
+    except mysql.connector.Error as error:
+        print(f"Error fetching data: {error}")
+        return []
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed")
+
+def update_expiry_date(fruit_id, expiry_date):
+    # Create the connection to the MySQL database
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='Hack',
+            user='root',
+            password='Akash003!'
+        )
+
+        if connection.is_connected():
+            print('Connected to MySQL database')
+
+            # Create a cursor object
+            cursor = connection.cursor()
+
+            # SQL query to update the expiry date for a specific fruit
+            update_query = "UPDATE fruits_spoilage SET Expiry_date = %s WHERE item_id = %s;"
+            cursor.execute(update_query, (expiry_date, fruit_id))
+
+            # Commit the changes
+            connection.commit()
+
+    except mysql.connector.Error as error:
+        print(f"Error updating data: {error}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed")
 
 def insert_data(data: List[dict]):
     insert_cmd = "INSERT INTO fruits_spoilage (item_name, Product_date, Expiry_date) VALUES (%s, %s, %s);"
@@ -36,6 +102,92 @@ def insert_data(data: List[dict]):
             connection.close()
             print("MySQL connection closed")
 
+def insert_data_initial(data: Union[List[Dict], Dict]):
+    insert_cmd = "INSERT INTO fruits_spoilage (item_name, Product_date, Quantity) VALUES (%s, %s, %s);"
+    
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='Hack',
+            user='root',
+            password='Akash003!'
+        )
+
+        if connection.is_connected():
+            print('Connected to MySQL database')
+
+            # Create a cursor object
+            cursor = connection.cursor()
+
+            # Ensure data is a list
+            if isinstance(data, dict):
+                data = [data]
+
+            # SQL query to insert each record
+            for record in data:
+                insert_query = (record['Item_name'], record['Production_date'], record['Quantity'])
+                cursor.execute(insert_cmd, insert_query)
+            
+            connection.commit()
+
+    except mysql.connector.Error as error:
+        print(f"Error inserting data: {error}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed")
+
+def insert_data_v2(data: Union[List[Dict], Dict]):
+    insert_cmd = "INSERT INTO fruits_spoilage (item_name, Product_date, Quantity) VALUES (%s, %s, %s);"
+    
+    try:
+        # Connect to MySQL
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='Hack',
+            user='root',
+            password='Akash003!'
+        )
+
+        if connection.is_connected():
+            print('Connected to MySQL database')
+
+            # Create a cursor object
+            cursor = connection.cursor()
+
+            # Ensure the data is in list form, even if it's a single record
+            if isinstance(data, dict):
+                data = [data]
+
+            # Log the received data for debugging purposes
+            print("Data received for insertion:", data)
+
+            # Get the current date (YYYY-MM-DD)
+            current_date = datetime.now().strftime('%Y-%m-%d')
+
+            # Insert each record
+            for record in data:
+                # Check if record is a dictionary and has the required keys
+                if isinstance(record, dict) and 'name' in record and 'quantity' in record:
+                    insert_query = (record['name'], current_date, record['quantity'])
+                    cursor.execute(insert_cmd, insert_query)
+                else:
+                    print(f"Invalid record format: {record}")
+            
+            # Commit the changes to the database
+            connection.commit()
+
+    except mysql.connector.Error as error:
+        print(f"Error inserting data: {error}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed")
+
 def Get_data():
     try:
         connection = mysql.connector.connect(
@@ -53,15 +205,8 @@ def Get_data():
 
             # Query to get the latest record for each unique item_name
             read_query = """
-                SELECT fs.item_name, fs.Product_date, fs.Expiry_date
-                FROM fruits_spoilage fs
-                INNER JOIN (
-                    SELECT item_name, MAX(Product_date) AS latest_product_date
-                    FROM fruits_spoilage
-                    GROUP BY item_name
-                ) latest_items 
-                ON fs.item_name = latest_items.item_name 
-                AND fs.Product_date = latest_items.latest_product_date;
+                SELECT item_name, Product_date, Expiry_date
+                FROM fruits_spoilage;
             """
             cursor.execute(read_query)
             rows = cursor.fetchall()
@@ -81,6 +226,58 @@ def Get_data():
                     'item_name': item_name,
                     'Product_date': Product_date,
                     'Expiry_date': Expiry_date,
+                }
+                json_data.append(data)
+
+            # Return Python list as JSON
+            return json_data
+
+    except mysql.connector.Error as error:
+        print(f"Error retrieving records: {error}")
+        return {"error": str(error)}
+
+    finally:
+        # Close cursor and connection
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+            print("MySQL connection closed")
+
+def Get_data_initial():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='Hack',
+            user='root',
+            password='Akash003!'
+        )
+
+        if connection.is_connected():
+            print('Connected to MySQL database')
+
+            # Create a cursor object
+            cursor = connection.cursor()
+
+            # Query to get the latest record for each unique item_name
+            read_query = """
+                SELECT item_name, Product_date FROM fruits_spoilage;
+            """
+            cursor.execute(read_query)
+            rows = cursor.fetchall()
+            json_data = []
+
+            # Iterate through rows and create JSON objects
+            for row in rows:
+                item_name, Product_date = row
+
+                # Convert date objects to strings
+                if isinstance(Product_date, date):
+                    Product_date = Product_date.isoformat()
+
+                data = {
+                    'item_name': item_name,
+                    'Product_date': Product_date,
                 }
                 json_data.append(data)
 
---
+++ frontend/src/pages/Storage.jsx
@@ -0,0 +1,173 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IoMdClose } from "react-icons/io";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import background from '/Users/akashbalaji/Desktop/Fruit_SpoilDetection/frontend/src/assets/BG_random2.png';
+
+function Storage() {
+    const [items, setItems] = useState([]);
+    const [newItem, setNewItem] = useState({ name: '', quantity: '' });
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleAddItem = () => {
+        if (newItem.name && newItem.quantity) {
+            setItems([...items, { ...newItem, quantity: parseFloat(newItem.quantity) }]); // Parse quantity to float
+            setNewItem({ name: '', quantity: '' });
+            setError(''); // Clear the error message on successful addition
+        } else {
+            setError('Please fill in both item name and quantity');
+        }
+    };    
+
+    const handleRemoveItem = (index) => {
+        const updatedItems = items.filter((_, i) => i !== index);
+        setItems(updatedItems);
+    };
+
+    const handleSubmit = async () => {
+        if (items.length === 0) {
+            setError('No items to submit. Please add at least one item.');
+        } else {
+            setError('');
+            try {
+                // Log the data being sent
+                console.log('Submitting items:', items);
+                
+                const response = await fetch('https://hchjn6x7-8000.inc1.devtunnels.ms/initial_storage', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(items),
+                });
+    
+                // Check for response status and log it
+                console.log('Response status:', response.status);
+    
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    console.log('Error details:', errorData); // Log server error details
+                    throw new Error('Failed to submit items');
+                }
+    
+                const result = await response.json();
+                console.log('Items submitted:', result.message);
+            } catch (error) {
+                console.error('Error submitting items:', error);
+            }
+        }
+    };
+    
+    
+
+    return (
+        <div className="min-h-screen flex">
+            {/* Left Sidebar */}
+            <div className="bg-maroon w-64 text-black flex flex-col justify-between" style={{ backgroundColor: "#f4f1de" }}>
+                <div>
+                    <h1 className="text-3xl p-3 font-bold">Frutech</h1>
+
+                    <div className='flex-col mt-5 gap-3'>
+                        <div className='hover:bg-[#d1ceba]' onClick={() => navigate("/dashboard")}>
+                            <h3 className="text-2xl p-3 font-medium">Inventory</h3>
+                        </div>
+
+                        <div className='hover:bg-[#d1ceba] bg-[#d1ceba]' onClick={() => navigate("/storage")}>
+                            <h3 className="text-2xl p-3 font-medium">Storage</h3>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Icon Container */}
+                <div className="flex justify-left pb-1" onClick={() => navigate("/")}>
+                    <MdOutlineAccountCircle style={{ fontSize: 33 }} />
+                    <h2 className="text-2xl font-light ml-2">Admin</h2>
+                </div>
+            </div>
+
+            {/* Main Content */}
+            <div className="flex-grow bg-cover bg-center relative" style={{
+                backgroundImage: `url(${background})`,
+                backgroundColor: 'rgba(0, 0, 0, 0.2)',
+                backgroundBlendMode: 'overlay'
+            }}>
+                <div className="bg-white bg-opacity-60 rounded-md mt-20 mx-10 p-6 shadow-lg">
+                    <div className="flex flex-col space-y-4">
+                        <h2 className="text-2xl font-semibold ml-2">Storage</h2>
+
+                        {/* Title Row */}
+                        <div className="flex p-4 rounded-md shadow-md" style={{ backgroundColor: "#f4f1de" }}>
+                            <div className="flex-1 font-bold">S.no</div>
+                            <div className="flex-1 font-bold">Item</div>
+                            <div className="flex-1 font-bold mr-7">Quantity (kgs)</div>
+                        </div>
+
+                        {/* Data Rows */}
+                        {items.map((item, index) => (
+                            <div key={index} className="flex bg-gray-200 p-4 rounded-md shadow-md" style={{ backgroundColor: "#FFFEF5" }}>
+                                <div className="flex-1">{index + 1}</div>
+                                <div className="flex-1">{item.name}</div>
+                                <div className="flex-1">{item.quantity}</div>
+                                <IoMdClose
+                                    style={{ fontSize: 25, opacity: 0.5, cursor: 'pointer' }}
+                                    onMouseEnter={(e) => e.target.style.color = 'red'}
+                                    onMouseLeave={(e) => e.target.style.color = 'black'}
+                                    onClick={() => handleRemoveItem(index)}
+                                />
+                            </div>
+                        ))}
+
+                        {/* Input Fields for new item */}
+                        <div className="flex flex-col bg-gray-100 p-4 rounded-md shadow-md">
+                            <div className="flex">
+                                <input
+                                    type="text"
+                                    className="flex-1 border rounded-md p-2 mr-2"
+                                    placeholder="Item name"
+                                    value={newItem.name}
+                                    onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+                                />
+                                <input
+                                    type="number"
+                                    step="0.01" // Allow decimals
+                                    className="flex-1 border rounded-md p-2 mr-2"
+                                    placeholder="Quantity (kgs)"
+                                    value={newItem.quantity}
+                                    onChange={(e) => setNewItem({ ...newItem, quantity: e.target.value })}
+                                />
+                                <button
+                                    type="submit"
+                                    className="bg-green-800 text-white py-2 px-4 rounded-lg hover:bg-green-900"
+                                    onClick={handleAddItem}
+                                >
+                                    Add
+                                </button>
+                            </div>
+
+                            {/* Error Message below input fields */}
+                            {error && (
+                                <div className="text-red-600 text-sm mt-2">
+                                    {error}
+                                </div>
+                            )}
+                        </div>
+
+                        {/* Submit Button */}
+                        <div className='flex justify-between mt-4'>
+                            <button
+                                type="submit"
+                                className="w-24 self-end bg-blue-800 text-white py-2 rounded-lg hover:bg-blue-900"
+                                onClick={handleSubmit}
+                            >
+                                Enter
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Storage;
